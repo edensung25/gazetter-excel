bb243fdd466fd25dbb993e753b5023df27d29938
Eden Sung <eden.sung@ufispace.com>
2018-12-19 14:56:59 -0500

    delete test data

--- vue/formatter.js
+++ vue/formatter.js
@@ -7,12 +7,6 @@ var app = new Vue({
             tableTbody: [],
             isAddPrefix: false,
             fileArr: [],
-            items:  [
-                      { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-                      { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-                      { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-                      { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-                    ],
             prefixContent:"",
             cate9: ['户数 Number of Households',
                     '男性人口 Male Population',
